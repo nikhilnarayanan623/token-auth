12adb2a507077a6e482c3b05964aee01d3fd586e
Nikhil N <nikhilnarayanan623@gmail.com>
2023-09-19 14:16:01 +0530

    Update token.go
    error value spells mistakes updated

--- token/token.go
+++ token/token.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-// Interface of token based authentication
+// Interface of token-based authentication
 type TokenAuth interface {
 	GenerateToken(req Payload) (TokenResponse, error)
 	VerifyToken(tokenString string) (Payload, error)
@@ -13,14 +13,14 @@ type TokenAuth interface {
 
 // Data stored in token
 type Payload struct {
-	TokenID  string // if not provided by user will create random id
+	TokenID  string // if not provided by the user will create a random id
 	UserID   interface{}
 	Email    string
 	Role     string
 	ExpireAt time.Time
 }
 
-// Response for the token generate
+// Response for the token generated
 type TokenResponse struct {
 	TokenID     string
 	TokenString string
@@ -31,17 +31,17 @@ func (p *Payload) GetUserIdAsUint() (uint, error) {
 
 	uid, ok := p.UserID.(uint)
 	if !ok {
-		return 0, errors.New("user id is not uint")
+		return 0, errors.New("user id is not in uint")
 	}
 	return uid, nil
 }
 
-// Get the payload user id as string
+// Get the payload user id as a string
 func (p *Payload) GetUserIdAsString() (string, error) {
 
 	str, ok := p.UserID.(string)
 	if !ok {
-		return "", errors.New("user id is not uint")
+		return "", errors.New("user id is not in string")
 	}
 	return str, nil
 }
